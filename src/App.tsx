a4d82d06d4d62f4c2fbdd3dec3d38999e22e39a6
Gamal Hal <gamalhal.chi@gmail.com>
2025-08-10 18:11:30 +0300

    initialize React project with Vite, TypeScript,

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Message from "./Message";
+
+function App () {
+
+  return (
+    <div >
+
+    <Message/>
+
+
+    </div> 
+
+
+
+
+
+  )
+
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message () {
+    const name = "";
+    if (name == "gamal") {
+    return (<h1>hellow {name}</h1>);
+}else {
+    return (<h1>hellow world</h1>);
+}
+}
+export default Message;
